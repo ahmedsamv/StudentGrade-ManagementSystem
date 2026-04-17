f91f929b5be50eaef38dcb0b03d84aa0bb7848e4
mohammednasef97-stack <mohammed.nasef97@gmail.com>
2026-04-17 15:39:10 -0700

    sourcing and bugs fixing

--- menu/subject_mange.sh
+++ menu/subject_mange.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/bash
+source ../subject_managment/addsubject
 
+source ../subject_managment/update_subject
+source ../subject_managment/list_subjects 
+source ../subject_managment/delete_subject
 
 function subject_manage() {
   
@@ -9,10 +13,10 @@ function subject_manage() {
     select choice in "Add Subject" "List Subjects"  "Update Subject" "Delete Subject" "Exit"
     do
         case $choice in
-            "Add Subject") echo 'stu' ;; 
-            "List Subjects") echo 'sub';;
-            "Update Subject") echo 'grad';; 
-            "Delete Subject") echo 'rep'  ;; 
+            "Add Subject")  add_subject ;; 
+            "List Subjects")  list_subjects;;
+            "Update Subject")  update_subject  ;; 
+            "Delete Subject")  delete_subject  ;; 
             "Exit") break ;;
             *) echo "Invalid choice" ;;
         esac
