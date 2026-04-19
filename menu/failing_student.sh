9cf7ba0ef90b109b61150857dbd930be56ffab91
sami <ahmedsamy121277@gmai.com>
2026-04-19 16:46:41 +0200

    create failing students function to show studends who have F and name this subject

---
+++ menu/failing_student.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/bash
+
+
+
+function failing_students() {
+
+    echo "----- Failing Students -----"
+
+    for stu in "../sgms_data/students"/*.stu
+    do
+        sid=$(sed -n '1p' "$stu")
+        name=$(sed -n '2p' "$stu")
+
+        for grd in "../sgms_data/grades"/*.grd
+        do
+            line=$(grep "^$sid|" "$grd")
+
+            if [[ -n "$line" ]]
+            then
+
+                grade=$(echo "$line" | cut -d'|' -f3)
+
+                if [[ "$grade" == "F" ]]
+                then
+                code=$(basename "$grd" .grd)
+                    echo "$name - $code"
+                    break
+                fi
+
+            fi
+        done
+
+    done
+}
+    
+failing_students
